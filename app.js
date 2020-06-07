4d18d6f4da010b52d563c7e006f6d41eb6b9c6ef
habibasiddiqui <habiba_siddiqui@ymail.com>
2020-06-07 17:53:18 +0500

    not done

---
+++ app.js
@@ -0,0 +1,70 @@
+
+
+
+// chapter 1
+
+// 1
+// alert ("Welcome to The Learners' Institute");
+
+// 2
+// window.alert ("Error! Please enter a valid password.");
+
+// 3
+// alert ("Welcome to JS Land...\nHappy Coding");
+
+// 4 
+// alert ("Welcome to JS Land...");
+// alert ("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+// 5 (written directly on browser console)
+// alert ("Hello... I can run JS through my web browser's console") ;
+
+// 6 (uncomment [chapter1, part6] in html & css files)
+// alert ("Welcome to my custom-built website!"); 
+
+// 7 (uncomment [chapter1, part7] in html & css files one at a time)
+// JS works the same in each case
+
+
+
+// chapter 2
+
+// 1
+// var username;
+
+// 2
+// var myName = "Habiba Sultana";
+
+//3 
+// var message = "Hello World";
+// alert (message);
+
+// 4
+// var studentName = "John Doe";
+// var studentAge = "15 years old";
+// var enrolledIn = "Certified Mobile Application Development"; 
+// alert (studentName);
+// alert (studentAge);
+// alert (enrolledIn);
+
+// 5
+// var pizza = "PIZZA";
+// alert (`${pizza} \n${pizza.slice (0, 4)} \n${pizza.slice (0,3)} \n${pizza.slice (0,2)} \n${pizza.slice (0,1)}`);
+// alert (pizza + "\n" + pizza.slice (0,4) + "\n" );
+
+// 6
+// var email = "habiba_sultana@outlook.com";
+// alert ("My email address is " + email);
+
+// 7
+// var book = "A Smarter Way to learn JavaScript" ;
+// alert ("I am trying to learn fro the book " + book);
+
+// 8 
+// document.write ("Yah! I can write HTML content through JavaScript");
+
+// 9
+var str = "-----";
+alert (str);
+
+
